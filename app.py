f88893ee22270e1b503e0fcb4f824a23da692fc8
Arturo-Gonzalez007 <114045288+Arturo-Gonzalez007@users.noreply.github.com>
2024-10-07 14:42:04 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+import sys
+import requests #pip install requests
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout) #pip install PyQt5
+
+from PyQt5.QtCore import Qt
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.user_input = QLabel("enter city: ", self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
+
+api_key = 'aacbd94389d8202d7d751038787ac835'
+
+
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+weather = weather_data.json()['weather'][0]['main']
+temp = round(weather_data.json()['main']['temp'])
+
+print(weather, temp)
