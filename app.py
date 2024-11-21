f749d8dc95ad2a7eb51128e77260c0e066aa5b8f
Arturo-Gonzalez007 <adglz007@icloud.com>
2024-11-21 11:36:45 -0600

    added daily high and low temperature and added their Celsius versions as well.

--- app.py
+++ app.py
@@ -14,9 +14,10 @@ class WeatherApp(QWidget):
         self.user_unit = QLabel("enter unit: 'F'/'C' ", self)
         self.unit_input = QLineEdit(self)
         self.get_weather_btn = QPushButton("Confirm", self)
-        self.temp_label = QLabel (self)
+        self.temp_label = QLabel(self)
         self.emoji_label = QLabel(self)
         self.description_label = QLabel(self)
+        self.temp_highLow = QLabel(self)
         self.initUI()
 
     def initUI(self):
@@ -32,6 +33,7 @@ class WeatherApp(QWidget):
         vbox.addWidget(self.temp_label)
         vbox.addWidget(self.emoji_label)
         vbox.addWidget(self.description_label)
+        vbox.addWidget(self.temp_highLow)
 
         self.setLayout(vbox)
 
@@ -42,12 +44,14 @@ class WeatherApp(QWidget):
         self.temp_label.setAlignment(Qt.AlignCenter)
         self.emoji_label.setAlignment(Qt.AlignCenter)
         self.description_label.setAlignment(Qt.AlignCenter)
+        self.temp_highLow.setAlignment(Qt.AlignCenter)
 
         self.user_city.setObjectName("user_city")
         self.city_input.setObjectName("city_input")
         self.user_unit.setObjectName("user_unit")
         self.unit_input.setObjectName("unit_input")
         self.temp_label.setObjectName("temp_label")
+        self.temp_highLow.setObjectName("temp_highLow")
         self.emoji_label.setObjectName("emoji_label")
         self.description_label.setObjectName("description_label")
         self.get_weather_btn.setObjectName("get_weather_btn")
@@ -77,6 +81,9 @@ class WeatherApp(QWidget):
             QLabel#temp_label{
                 font-size: 60px;
             }
+            QLabel#temp_highLow{
+                font-size: 30px;
+            }
             QLabel#emoji_label{
                 font-size: 100px;
                 font-family: Segoe UI emoji;
@@ -101,6 +108,7 @@ class WeatherApp(QWidget):
                 f"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&APPID={api_key}")
             weather_data.raise_for_status()
             weather = weather_data.json() # Test commit by omar\
+            print(weather)
 
             if weather["cod"] == 200:
                 self.display_weather(weather)
@@ -140,24 +148,30 @@ class WeatherApp(QWidget):
         self.temp_label.setText(message)
         self.emoji_label.setText(" ")
         self.description_label.setText(" ")
+        self.temp_highLow.setText(" ")
+
 
     def display_weather(self, weather):
         self.temp_label.setStyleSheet("font-size: 60px")
 
         unit = self.unit_input.text()
-        weather_f = weather["main"]["temp"]
-        weather_c = ((weather_f - 32)*5)/9
+        weather_temp = weather["main"]["temp"]
+        weather_high = weather["main"]["temp_max"]
+        weather_low = weather["main"]["temp_min"]
         weather_id = weather["weather"][0]["id"]
         weather_description = weather["weather"][0]["description"]
 
         if unit == "F":
-            self.temp_label.setText(f"{weather_f:.0f}°F")
+            self.temp_label.setText(f"{weather_temp:.0f}°F")
             self.emoji_label.setText(self.get_emoji(weather_id))
             self.description_label.setText(weather_description)
+            self.temp_highLow.setText(f"H:{weather_high:.0f}° L:{weather_low:.0f}°")
         elif unit == "C":
-            self.temp_label.setText(f"{weather_c:.0f}°C")
+            self.temp_label.setText(f"{((weather_temp - 32) * 5 / 9):.0f}°C")
+            print(weather_temp)
             self.emoji_label.setText(self.get_emoji(weather_id))
             self.description_label.setText(weather_description)
+            self.temp_highLow.setText(f"H:{((weather_high - 32) * 5 / 9):.0f}° L:{((weather_low - 32) * 5 / 9):.0f}°")
         else:
             self.display_error("Invalid unit:\nPlease try again")
 
